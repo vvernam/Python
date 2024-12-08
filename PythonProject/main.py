c71a3b4c25dc277d8e4fd468f7f13df0f3c65dab
vvernam <maksim.gold17@gmail.com>
2024-12-08 19:55:41 +1000

    task1.3

---
+++ PythonProject/main.py
@@ -0,0 +1,21 @@
+#1st
+
+#1st program
+print(9**0.5*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(2+2*2 == (2+2)*2)
+#4th program
+print(str(int(float('123.456')*10)%10))
+name = 'maksim'
+
+#2nd
+
+print (name)
+age = 16
+print (age)
+age += 5
+print (age)
+is_student = True
+print (is_student)
