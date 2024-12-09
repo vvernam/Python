95837c4571749f19f5c5bb5b4d7614381db67d96
vvernam <maksim.gold17@gmail.com>
2024-12-09 16:38:35 +1000

    mod1_tas3-5

--- PythonProject/mod1_tas3.py
+++ PythonProject/mod1_tas3.py
@@ -2,4 +2,4 @@ homework = 12
 time = 1.5
 name = 'Python'
 t_of_hw = time / homework
-print ('Курс:' , name , ',' , 'всего задач:' , homework , ',' , 'затрачено часов' , time , ',' , 'среднее время выполнения' , t_of_hw)
+print ('Курс:', name, ', всего задач:', homework, ', затрачено часов:', time, ', среднее время выполнения',t_of_hw)
---
+++ PythonProject/mod1_tas4.py
@@ -0,0 +1,6 @@
+example = "alekseus"
+print(example[0])
+print(example[-1])
+print(example[4:9])
+print(example[::-1])
+print(example[::2])
